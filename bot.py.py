fa74eb2815ed447d1d47d697b6eb5895bd4558ec
Muhammed <muhammedalsharif575@gmail.com>
2026-06-14 02:48:54 +0300

    Fix port binding error

--- bot.py.py
+++ bot.py.py
@@ -1,4 +1,5 @@
 import telebot
+from keep_alive import keep_alive
 
 TOKEN = '8775190237:AAGFp6jlqlJ-lOK7PuqOD6qQlS__8NEMTi4'
 bot = telebot.TeleBot(TOKEN)
@@ -8,4 +9,5 @@ def send_welcome(message):
     bot.reply_to(message, "مرحباً بك في بوت دليل القبول! أنا جاهز لمساعدتك.")
 
 print("البوت يعمل الآن...")
+keep_alive()
 bot.infinity_polling()
